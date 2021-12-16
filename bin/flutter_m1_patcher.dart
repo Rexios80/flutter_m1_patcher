c7f7a71f91a5d8e6924ef05229e8dc342291fe7a
Rexios <rexios@rexios.dev>
2021-12-16 00:44:38 -0500

    Got rid of some console commands

--- bin/flutter_m1_patcher.dart
+++ bin/flutter_m1_patcher.dart
@@ -1,32 +1,59 @@
 import 'dart:io';
 
 void main(List<String> arguments) async {
+  // Get the path to Flutter
   final result = await Process.run('which', ['flutter']);
   final flutterPath = result.stdout as String;
-  final flutterBinFolder = File(flutterPath.trim()).parent;
-  final flutterBinCachePath = '${flutterBinFolder.path}/cache';
+
+  // Ask the user for confirmation
+  stdout.write('Flutter found at $flutterPath');
+  stdout.write('Continue? (y/n) ');
+  final confirmation = stdin.readLineSync();
+  if (confirmation != 'y') {
+    print('Aborting');
+    return;
+  }
+
+  // Get Flutter's Dart SDK vserion
+  final flutterBinCachePath = File(flutterPath.trim()).parent.path + '/cache';
   final dartSdkVersionFile = File('$flutterBinCachePath/dart-sdk/version');
   final dartSdkVersion = dartSdkVersionFile.readAsStringSync().trim();
-  await Process.run('rm', ['-rf', '$flutterBinCachePath/dart-sdk']);
-  // Download dart sdk
-  await Process.run('curl', [
-    '-o',
-    '$flutterBinCachePath/dart-sdk.zip',
-    'https://storage.googleapis.com/dart-archive/channels/stable/release/$dartSdkVersion/sdk/dartsdk-macos-arm64-release.zip'
-  ]);
-  // Unzip dart sdk
-  await Process.run('unzip', [
-    '-o',
-    '$flutterBinCachePath/dart-sdk.zip',
-    '-d',
-    flutterBinCachePath
-  ]);
-  // Remove zip file
-  await Process.run('rm', ['-rf', '$flutterBinCachePath/dart-sdk.zip']);
-  // Link the
-  await Process.run('ln', [
-    '-f',
-    '$flutterBinCachePath/dart-sdk/bin/snapshots/frontend_server.dart.snapshot',
-    '$flutterBinCachePath/artifacts/engine/darwin-x64/frontend_server.dart.snapshot'
-  ]);
+
+  // Delete the existing dart-sdk folder
+  print('Deleting bundled Dart SDK...');
+  Directory('$flutterBinCachePath/dart-sdk').deleteSync(recursive: true);
+
+  // Download the Dart SDK
+  print('Downloading Dart SDK $dartSdkVersion for macOS arm64...');
+  await Process.run(
+    'curl',
+    [
+      '-o',
+      '$flutterBinCachePath/dart-sdk.zip',
+      'https://storage.googleapis.com/dart-archive/channels/stable/release/$dartSdkVersion/sdk/dartsdk-macos-arm64-release.zip'
+    ],
+  );
+
+  // Unzip the Dart SDK
+  print('Unzipping Dart SDK...');
+  await Process.run(
+    'unzip',
+    ['-o', '$flutterBinCachePath/dart-sdk.zip', '-d', flutterBinCachePath],
+  );
+
+  // Delete the zip file
+  print('Deleting zip file...');
+  File('$flutterBinCachePath/dart-sdk.zip').deleteSync();
+
+  // Delete the existing engine frontend_server.dart.snapshot file
+  print('Deleting engine frontend_server.dart.snapshot file...');
+  File('$flutterBinCachePath/artifacts/engine/darwin-x64/frontend_server.dart.snapshot')
+      .deleteSync();
+
+  // Copy the dart-sdk frontend_server.dart.snapshot file to the engine folder
+  print('Copying Dart SDK frontend_server.dart.snapshot file to engine...');
+  File('$flutterBinCachePath/dart-sdk/bin/snapshots/frontend_server.dart.snapshot')
+      .copySync(
+    '$flutterBinCachePath/artifacts/engine/darwin-x64/frontend_server.dart.snapshot',
+  );
 }
